e71240760d765a2a4d40e6dccd2fea914bac8014
2vnkt <2vnkt9@gmail.com>
2023-09-21 18:24:28 +0530

    added multiple same case for push job

--- cypress/e2e/basic_test.cy.js
+++ cypress/e2e/basic_test.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('case-1', () => {
     cy.visit('https://example.cypress.io')
     cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
     cy.wait(5000)
@@ -7,4 +7,32 @@ describe('template spec', () => {
     cy.wait(5000)
     cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
   })
+
+  it('Case-2', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+    cy.wait(5000)
+    cy.get('.navbar-brand').click()
+    cy.wait(5000)
+    cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+  })
+
+  it('pCase-3', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+    cy.wait(5000)
+    cy.get('.navbar-brand').click()
+    cy.wait(5000)
+    cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+  })
+
+  it('Case-4', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+    cy.wait(5000)
+    cy.get('.navbar-brand').click()
+    cy.wait(5000)
+    cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+  })
+
 })
