1fde021d8e810280ac1b99427f860a41bcb0a6dc
2vnkt <2vnkt9@gmail.com>
2023-09-22 16:34:07 +0530

    added new yml for cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "4urwvt",
+  CYPRESS_RECORD_KEY:"a389ed13-2c1f-4f6c-81de-d39b04f1dd57",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
