2e5377d22427c2fabece8b1a8bf9d02a34597f9e
2vnkt <2vnkt9@gmail.com>
2023-09-22 15:59:04 +0530

    added folders and files, run specific spec

---
+++ cypress/e2e/sanity/sanity1.cy.js
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+    it('Sanity1 case-1', () => {
+      cy.visit('https://example.cypress.io')
+      cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+      cy.wait(5000)
+      cy.get('.navbar-brand').click()
+      cy.wait(5000)
+      cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+    })
+    it('Sanity1 case-2', () => {
+        cy.visit('https://example.cypress.io')
+        cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+        cy.wait(5000)
+        cy.get('.navbar-brand').click()
+        cy.wait(5000)
+        cy.get('#navbar > :nth-child(1) > :nth-child(2) > a').should('be.visible')
+      })
+})
