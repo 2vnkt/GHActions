59891cc0b0455466c5cfe6fc79f9207af37b555b
2vnkt <2vnkt9@gmail.com>
2023-10-05 11:42:31 +0530

    removed key from config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "4urwvt",
-  CYPRESS_RECORD_KEY:"a389ed13-2c1f-4f6c-81de-d39b04f1dd57",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
